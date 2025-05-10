24f19e3d32cc976ea100a1955519b6d4effb3376
QueenieAnnastasia <94047113+QueenieAnnastasia@users.noreply.github.com>
2025-05-10 23:19:20 +0700

    Area of Rectangle
    Thực hành tính diện tích hình chữ nhật

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //int height = 6;
+        //int width = 4;
+        Console.Write("Nhập chiều dài: ");
+        int height = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nhập chiều rộng: ");
+        int width = Convert.ToInt32(Console.ReadLine());
+
+        int areaOfRectangle = height*width;
+        Console.WriteLine("Diện tích hình chữ nhật là " + areaOfRectangle);
+    }
+}
